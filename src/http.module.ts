915bb7b3d82efb57408bb8ff14f702ed578191d0
Pedro Guilherme <pedroguinasc2002@gmail.com>
2023-09-12 16:22:33 -0300

    first commit

---
+++ src/http.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  imports: [HttpModule],
+  controllers: [AppController],
+  providers: [PrismaService]
+})
+export class HttpModule {}
